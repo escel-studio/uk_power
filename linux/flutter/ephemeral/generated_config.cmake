40ec230c77031a50cc40a62527a6f4b504ecdf80
Kadzup <38593867+Kadzup@users.noreply.github.com>
2022-03-08 02:35:30 +0200

    Linux

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,14 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/kadzup/dev/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/kadzup/dev/uk_power" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/kadzup/dev/flutter"
+  "PROJECT_DIR=/home/kadzup/dev/uk_power"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/kadzup/dev/uk_power/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
